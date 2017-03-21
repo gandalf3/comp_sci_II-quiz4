be86b7e15458bc539f8c4907a5227d2727775470
gandalf3 <gandalf3@blendermonkey.com>
2017-03-21 14:03:46 -0700

    construct courses

--- Course.java
+++ Course.java
@@ -2,6 +2,15 @@ class Course {
     String title;
     String subject;
     String description;
+    ArrayList<Course> prereqs;
+    int crn;
 
+    public Course(String title) {
+      this.title = title;
+    }
+    public Course(String title, String subject) {
+      this(title);
+      this.subject = subject;
+    }
 
 }
